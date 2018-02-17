979eab798cc03445f6c02cf0eddea605d4c2c25d
Octavector <hello@octavector.co.uk>
2018-02-17 20:48:05 +0000

    More JavaScript tidy up, remove debug logs

--- octopus.js
+++ octopus.js
@@ -1,57 +1,48 @@
-window.addEventListener("load", function(e){
+window.addEventListener("load", function (e) {
 
     const stat = { playing: 0, current: 0 };
 
-    //the hadler for our proxy allows us to hook onto 'get' and 'set' requests
+    //the handler for our proxy allows us to hook onto 'get' and 'set' requests
     const handler_stat = {
-        get: function(target, propKey) {
+        get: function (target, propKey) {
             //get trap
-            console.log('get ' + propKey);
-           // console.log(`GET: target: ${target} - propKey: ${propKey}`);
-           current.textContent = convertToMinutes(vid.currentTime);
+            current.textContent = convertToMinutes(vid.currentTime);
             return Reflect.get(target, propKey);
         },
-        set: function(target, propKey, value) {
+        set: function (target, propKey, value) {
             //set trap
-            console.log('set ' + propKey);
-            console.log(`SET: target: ${target} - propKey: ${propKey} - value: ${value}`);
             Reflect.set(target, propKey, value);
-            //vid.currentTime = newRuntime;
-            //vid.currentTime = proxy_stat.current;
         }
     };
     //creating a proxy of stat to intercept requests for properties
     const proxy_stat = new Proxy(stat, handler_stat);
 
+    //grab DOM elements
     const vid = document.querySelector('.octopus_video video');
-    var playBtn = document.querySelector('.octopus_btn .btnPlayPause');
+    const playBtn = document.querySelector('.octopus_btn .btnPlayPause');
     const timeline = document.querySelector('.octopus_timeline');
     const progress = document.querySelector('.octopus_progress');
     const current = document.querySelector('.octopus_current');
     const duration = document.querySelector('.octopus_duration');
-    
 
     //event listeners
     vid.addEventListener('playing', (e) => { proxy_stat.playing = 1; });
     vid.addEventListener('pause', (e) => { proxy_stat.playing = 0; });
     //Chrome requires 'loadeddata', Firefox does not
     //If we cant grab vid.duration on load then we wait for 'loadeddata' event (chrome)
-    vid.duration ? duration.textContent = convertToMinutes(vid.duration) : vid.addEventListener('loadeddata',(e) => {
+    vid.duration ? duration.textContent = convertToMinutes(vid.duration) : vid.addEventListener('loadeddata', (e) => {
         duration.textContent = convertToMinutes(vid.duration);
     });
-
 
     playBtn.addEventListener('click', playVid);
     timeline.addEventListener('click', track);
 
     //convert video duration in seconds 'total' to minutes and seconds, add to UI
-    function convertToMinutes(total){
+    function convertToMinutes(total) {
         let minutes = Math.floor(total / 60);
         let seconds = Math.floor(total - minutes * 60);
         return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
     }
-
-    
 
     //returns the percentage difference between 'num1' and 'num2'
     function getPercent(num1, num2) {
@@ -66,35 +57,30 @@ window.addEventListener("load", function(e){
         return decimal * target;
     }
 
+    //track function manages clicks on timeline
     function track(e) {
         const target = e.target;
         rect = target.getBoundingClientRect(),
             [offsetX, offsetY] = [e.clientX - rect.left, e.clientY - rect.top];
 
-        console.log([offsetX, offsetY]);
-        console.log(progress);
-
         // get percentage of offsetX to line width
         // match this to currentTime vs duration
         progress.style.width = `${getPercent(offsetX, rect.width)}%`
-        console.log('percent ' + getPercent(offsetX, rect.width));
         let newRuntime = percentOff(getPercent(offsetX, rect.width), vid.duration);
-        console.log('runtime: ' + newRuntime);
         //jump to new location in video
         vid.currentTime = newRuntime;
         //proxy_stat.current = newRuntime;
     }
 
-
     //toggle play and pause functionality and btn image
     function playVid() {
-        if(proxy_stat.playing !== 1){
+        if (proxy_stat.playing !== 1) {
             vid.play();
             playBtn.style.backgroundImage = "url('img/btn_pause.svg')";
-        }else{
+        } else {
             vid.pause();
             playBtn.style.backgroundImage = "url('img/btn_play.svg')";
-        } 
+        }
     }
 
     //begin animation loop
